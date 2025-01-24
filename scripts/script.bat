705e5fa3e4ba8e9f550b0e51844d6f1178f77ba6
fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2025-01-24 10:01:05 +0900

    chore: makeファイルを修正
    登録にはregsrvを使うように戻す (#3)

--- scripts/script.bat
+++ scripts/script.bat
@@ -1,5 +1,7 @@
 @REM net use Z: "\\vmware-host\Shared Folders" /persistent:yes
+regsvr32.exe "D:\azookey-windows\target\debug\azookey_windows.dll" /u /s
+regsvr32.exe "D:\azookey-windows\target\i686-pc-windows-msvc\debug\azookey_windows.dll" /u /s
 start D:\azookey-windows\target\debug\azookey-server.exe
 start D:\azookey-windows\target\debug\ui.exe
-"C:\Program Files\Azookey\unins000.exe"
-target\release\azookey-installer.exe
+regsvr32.exe "D:\azookey-windows\target\debug\azookey_windows.dll" /s
+regsvr32.exe "D:\azookey-windows\target\i686-pc-windows-msvc\debug\azookey_windows.dll" /s

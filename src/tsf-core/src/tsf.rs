62a42620fda9c27962b2dac168d206585e9ec4f9
fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2026-07-26 11:26:10 +0900

    typoを修正

--- src/tsf-core/src/tsf.rs
+++ src/tsf-core/src/tsf.rs
@@ -6,7 +6,7 @@ pub(super) mod edit_session;
 pub(super) mod key_event_sink;
 pub(super) mod language_bar;
 pub(super) mod source;
-pub(super) mod text_input_proccesor;
+pub(super) mod text_input_processor;
 pub(super) mod text_layout_sink;
 pub(super) mod text_service;
 pub(super) mod text_service_factory;
